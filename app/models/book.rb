1e22c52d8d9ced693b86c06d5edd18b467c1223c
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-09-10 11:00:57 -0400

    Step 4

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ActiveRecord::Base
   validates :title, presence: true
   validates :author, presence: true
   validates :favorite, inclusion: { in: [true, false] }
+  validates :rating, numericality: { greater_than: -1, less_than: 101 }, allow_blank: true
 end
