57d9345eee78a4c089529f2e35e51aac65adc802
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-09-10 11:47:37 -0400

    Step 8

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   has_many :checkouts
-
+  has_many :categorizations
+  has_many :categories, through: :categorizations
   validates :title, presence: true
   validates :author, presence: true
   validates :rating, numericality: { greater_than: -1, less_than: 101 }, allow_blank: true
